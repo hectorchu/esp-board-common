77b2ea9b70ad045cce94329ed17c8b760284f4d3
Hector Chu <hectorchu@gmail.com>
2026-05-06 21:14:16 +0100

    build in seedsigner python files

--- include/mpconfig_esp32.h
+++ include/mpconfig_esp32.h
@@ -1,6 +1,10 @@
 #define MICROPY_TASK_STACK_SIZE             (32 * 1024)
+#define MICROPY_PY_SYS_EXC_INFO             (1)
 
 #include <mpconfigport.h>
+
+#undef MICROPY_FLOAT_IMPL
+#define MICROPY_FLOAT_IMPL                  (MICROPY_FLOAT_IMPL_DOUBLE)
 
 #undef MICROPY_PY_MACHINE_ADC
 #undef MICROPY_PY_MACHINE_ADC_BLOCK
