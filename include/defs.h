03b04a8f5b38720c6faf58f4a9daf90da366fa82
Hector Chu <hectorchu@gmail.com>
2026-05-11 23:02:11 +0100

    use builtin lv malloc

--- include/defs.h
+++ include/defs.h
@@ -1,4 +1,7 @@
+#define LV_CONF_H
+#define LV_MEM_POOL_ALLOC malloc
+#define LV_MEM_SIZE 3000000
 #define LV_USE_LODEPNG 1
+#define LV_USE_OS LV_OS_FREERTOS
 #define LV_USE_PRIVATE_API 1
-#define LV_USE_STDLIB_MALLOC LV_STDLIB_CLIB
 #define MP_CONFIGFILE "mpconfig_esp32.h"
