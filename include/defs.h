0c223e63e1140f63f54ddedee79f8637716fcf2b
Hector Chu <hectorchu@gmail.com>
2026-05-16 16:54:14 +0100

    move lv defs to lv_conf

--- include/defs.h
+++ include/defs.h
@@ -1,7 +1,2 @@
-#define LV_CONF_H
-#define LV_MEM_POOL_ALLOC malloc
-#define LV_MEM_SIZE 3000000
-#define LV_USE_LODEPNG 1
-#define LV_USE_OS LV_OS_FREERTOS
-#define LV_USE_PRIVATE_API 1
+#define LV_CONF_PATH "lv_conf.h"
 #define MP_CONFIGFILE "mpconfig_esp32.h"
